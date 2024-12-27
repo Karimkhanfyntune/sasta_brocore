59f680a5353c7585f1881331ce943bf07e760287
karim khan <abdulkarim.khan@fyntune.com>
2024-12-27 15:27:41 +0530

    asd

---
+++ src/main/java/com/example/demo/SastaBrocoreApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { 
+		@Server(url = "http://localhost:8080", description = "Generated Server URL")
+
+	 })
+
+@SpringBootApplication
+public class SastaBrocoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SastaBrocoreApplication.class, args);
+	}
+
+}
